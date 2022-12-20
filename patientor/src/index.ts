4dcb04448a455abf62ad79b1e935505272c68fa8
Faiz Alam <faizalam9883@gmail.com>
2022-12-20 23:10:04 +0530

    Exercises 9.10 9.11
    took 1.5 hours

--- patientor/src/index.ts
+++ patientor/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import diagnosesRouter from "./routes/diagnoses";
+import patientRouter from "./routes/patients";
 import cors from "cors";
 
 const app = express();
@@ -12,5 +14,8 @@ const PORT = 3001;
 app.get("/api/ping", (_req, res) => {
 	res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosesRouter);
+app.use("/api/patients", patientRouter);
 
 app.listen(PORT, () => console.log(`Server running at PORT ${PORT}`));

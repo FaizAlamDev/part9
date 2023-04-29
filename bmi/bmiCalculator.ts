727d8b24833e6d8431956ca1365b057efad320a7
Faiz Alam <faizalam9883@gmail.com>
2023-04-30 01:40:54 +0530

    Exercise 9.4 9.5
    took 2 hours

--- bmi/bmiCalculator.ts
+++ bmi/bmiCalculator.ts
@@ -1,6 +1,4 @@
-import { isNotNumber } from "./utils";
-
-function calculateBmi(height: number, weight: number) {
+export default function calculateBmi(height: number, weight: number) {
   const bmi = weight / Math.pow(height / 100, 2);
   if (bmi < 18.5) {
     return "Underweight";
@@ -10,21 +8,3 @@ function calculateBmi(height: number, weight: number) {
     return "Overweight";
   }
 }
-
-const parseArguments = (args: string[]) => {
-  if (args.length < 4) throw new Error("Not Enough Arguments");
-  if (args.length > 4) throw new Error("Too many arguments");
-
-  if (!isNotNumber(args[2]) && !isNotNumber(args[3])) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
-    };
-  } else {
-    throw new Error("Arguments are not of the correct type");
-  }
-};
-
-const { height, weight } = parseArguments(process.argv);
-
-console.log(calculateBmi(height, weight));
--- bmi/utils.ts
+++ bmi/utils.ts
@@ -1,1 +1,15 @@
 export const isNotNumber = (argument: string) => isNaN(Number(argument));
+
+export const parseArgumentsBmiCalculator = (args: string[]) => {
+  if (args.length < 4) throw new Error("Not Enough Arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNotNumber(args[2]) && !isNotNumber(args[3])) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Arguments are not of the correct type");
+  }
+};

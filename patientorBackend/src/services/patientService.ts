0947e77c6f574a42af9779c30c7d6af541d58e57
Faiz Alam <faizalam9883@gmail.com>
2023-04-30 19:42:12 +0530

    Exercise 9.8 9.9 9.10 9.11
    took 4 hours

---
+++ patientorBackend/src/services/patientService.ts
@@ -0,0 +1,18 @@
+import patientData from "../data/patients";
+import { NonSensitivePatient, Patient } from "../types";
+
+const getEntries = (): Patient[] => {
+  return patientData;
+};
+
+const getNonSensitiveEntries = (): NonSensitivePatient[] => {
+  return patientData.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default { getEntries, getNonSensitiveEntries };

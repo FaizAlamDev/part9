63dfd7614faeeb7cc191cfc920eaddc28c6776ee
Faiz Alam <faizalam9883@gmail.com>
2023-05-03 00:06:13 +0530

    Exercise 9.15
    took 2.5 hours

--- courseinfo/src/App.tsx
+++ courseinfo/src/App.tsx
@@ -1,23 +1,101 @@
+import assertNever from "./utils";
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface coursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartBasic extends coursePartDescription {
+  kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+interface CoursePartBackground extends coursePartDescription {
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+interface CoursePartSpecial extends coursePartDescription {
+  requirements: string[];
+  kind: "special";
+}
+
+type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground
+  | CoursePartSpecial;
+
 const Header = ({ name }: { name: string }) => {
   return <h1>{name}</h1>;
 };
 
-const Content = ({
-  courseParts,
-}: {
-  courseParts: { name: string; exerciseCount: number }[];
-}) => {
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.kind) {
+    case "basic":
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          {part.description}
+        </p>
+      );
+    case "group":
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          project exercises {part.groupProjectCount}
+        </p>
+      );
+    case "background":
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          {part.description}
+          <br />
+          submit to {part.backgroundMaterial}
+        </p>
+      );
+
+    case "special":
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          {part.description}
+          <br />
+          required skills: {part.requirements.map((r) => r + ",")}
+        </p>
+      );
+    default:
+      return assertNever(part);
+  }
+};
+
+const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
   return (
     <>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
+      {courseParts.map((part) => (
+        <Part key={part.name} part={part} />
+      ))}
     </>
   );
 };
@@ -28,18 +106,45 @@ const Total = ({ count }: { count: number }) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial:
+        "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special",
     },
   ];
 
